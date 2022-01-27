c778af60390c758ac5dad736714d63e1d0a73984
krutoydev <starkmarvel074@gmail.com>
2022-01-27 18:16:11 +0500

    Project done

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.autoWrite', {
+    strings: [
+        "Olloshukur",
+        "Web Developer",
+        "UI Designer",
+        "Node Js Developer"
+    ],
+
+    typeSpeed: 10,
+    backSpeed: 12,
+    loop: true
+});
